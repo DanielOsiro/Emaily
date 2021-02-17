56bec5000af5af0931dfddec5606413ce99cbfe6
Daniel Osiro <danielosiro@hotmail.com>
2021-02-17 07:45:58 -0300

    Refactored passport service to user async/await

--- services/passport.js
+++ services/passport.js
@@ -19,16 +19,13 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
     callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    console.log({ accessToken, refreshToken, profile });
+}, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id });
 
-    User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-            done(null, existingUser);
-        } else {
-            new User({ googleId: profile.id }).save().then(user => {
-                done(null, user);
-            });
-        }
-    });
+    if (existingUser) {
+        return done(null, existingUser);
+    }
+    
+    const user = await new User({ googleId: profile.id }).save();
+    done(null, user);
 }));
