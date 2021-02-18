5141719eca7de600c481229c2c9c8a1eff1e2660
Daniel Osiro <danielosiro@hotmail.com>
2021-02-18 20:58:01 -0300

    [server] Installed body-parser middleware

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// Express middlewares
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_KEY],
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
