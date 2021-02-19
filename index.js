433190c06ce9e3ac7a17d93a67f7e4105e538421
Daniel Osiro <danielosiro@hotmail.com>
2021-02-18 23:29:13 -0300

    [server] Configured server side to serve client

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ mongoose.connect(process.env.MONGODB_URI);
 // Generate express routes
 require("./routes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets. E.g.:main.js file, main.css file
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index index.html file if it doesn't recognize the route
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Port express is running on
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
